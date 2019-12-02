c86751498c33207a342c5b921a75e64f4033a9c6
Romário Beckman <romabeckman@gmail.com>
2019-12-02 17:08:11 -0300

    Update SendSmsZenvia.php

--- SendSmsZenvia.php
+++ SendSmsZenvia.php
@@ -8,7 +8,7 @@ class SendSmsZenvia {
     protected $curl;
 
     private $CONTA = ''; // informe a conta 
-    private $SENHA = '';
+    private $SENHA = ''; // informe a senha 
 
     private $apiKey; //BASE64 conta|senha
     private $apiUrl = "https://api-rest.zenvia.com/services/send-sms";
@@ -20,34 +20,37 @@ class SendSmsZenvia {
         $this->remetente = $remetente;
         $this->apiKey = base64_encode($this->CONTA . ':' . $this->SENHA);
     }
+    
+    protected function exec(string $json): string {
+        $curl = curl_init();
+        
+        curl_setopt($curl, CURLOPT_URL, $this->apiUrl);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_POST, true);
 
-    private function cabecalho() {
-        $this->curl = curl_init();
-        curl_setopt($this->curl, CURLOPT_URL, $this->apiUrl);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->curl, CURLOPT_HEADER, false);
-        curl_setopt($this->curl, CURLOPT_POST, true);
-
-        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
             "Authorization: Basic {$this->apiKey}",
             "Accept: application/json",
         ));
+        
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+        $response = curl_exec($curl);
+        curl_close($curl);
+        return $response;
     }
 
     protected function enviar(string $json): bool {
-        $this->cabecalho();
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $json);
-        $response = curl_exec($this->curl);
-        curl_close($this->curl);
+        $response = $this->exec($json);
 
         $this->response = json_decode($response);
 
         if (isset($this->response->sendSmsResponse)) {
             $this->response = $this->response->sendSmsResponse;
-            return (BOOL) ((INT) $this->response->statusCode == Zenvia::STATUS_CODE_OK);
+            return (bool) ((int) $this->response->statusCode == self::STATUS_CODE_OK);
         } else
-            return FALSE;
+            return false;
     }
 
     public function envioUnico(int $numero, string $mensagem, \DateTime $data): bool {
