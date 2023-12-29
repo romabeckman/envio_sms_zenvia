8c23a364c73fa8388176f5d0d73f00f9b9e83d9e
Romário Beckman <romabeckman@gmail.com>
2023-12-28 23:07:02 -0300

    Update SendSmsZenvia.php

--- SendSmsZenvia.php
+++ SendSmsZenvia.php
@@ -1,75 +1,158 @@
 <?php
 
+/**
+ * Class SendSmsZenvia
+ *
+ * A class for sending SMS using the Zenvia API.
+ */
 class SendSmsZenvia {
 
+    /**
+     * Country code for Brazil.
+     */
     const DDI_BR = 55;
 
-    protected $response;
-    protected $curl;
+    /**
+     * Zenvia API base URL.
+     */
+    private string $apiUrl = "https://api-rest.zenvia.com/services/send-sms";
 
-    private $CONTA = ''; // informe a conta 
-    private $SENHA = ''; // informe a senha 
+    /**
+     * Zenvia account.
+     * @var string
+     */
+    private string $account;
 
-    private $apiKey; //BASE64 conta|senha
-    private $apiUrl = "https://api-rest.zenvia.com/services/send-sms";
-    private $remetente;
+    /**
+     * Zenvia password.
+     * @var string
+     */
+    private string $password;
 
+    /**
+     * Zenvia API key (BASE64 encoded account and password).
+     * @var string
+     */
+    private string $apiKey;
+
+    /**
+     * SMS sender.
+     * @var string
+     */
+    private string $sender;
+
+    /**
+     * Response from the API.
+     * @var object|null
+     */
+    protected ?object $response = null;
+
+    /**
+     * HTTP client.
+     * @var resource|null
+     */
+    private ?resource $curl = null;
+
+    /**
+     * Status code for successful API response.
+     */
     const STATUS_CODE_OK = 0;
 
-    public function __construct(string $remetente) {
-        $this->remetente = $remetente;
-        $this->apiKey = base64_encode($this->CONTA . ':' . $this->SENHA);
+    /**
+     * SendSmsZenvia constructor.
+     *
+     * @param string $sender SMS sender.
+     */
+    public function __construct(string $sender) {
+        $this->sender = $sender;
+        $this->account = getenv('ZENVIA_ACCOUNT'); // Update with your .env variable
+        $this->password = getenv('ZENVIA_PASSWORD'); // Update with your .env variable
+        $this->apiKey = base64_encode($this->account . ':' . $this->password);
     }
-    
-    protected function exec(string $json): string {
-        $curl = curl_init();
-        
-        curl_setopt($curl, CURLOPT_URL, $this->apiUrl);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_POST, true);
 
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    /**
+     * Execute the HTTP request.
+     *
+     * @param string $json JSON data to send.
+     * @return string API response.
+     */
+    protected function execute(string $json): string {
+        $this->curl = curl_init();
+
+        curl_setopt($this->curl, CURLOPT_URL, $this->apiUrl);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_HEADER, false);
+        curl_setopt($this->curl, CURLOPT_POST, true);
+
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
             "Authorization: Basic {$this->apiKey}",
             "Accept: application/json",
         ));
-        
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
-        $response = curl_exec($curl);
-        curl_close($curl);
+
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $json);
+        $response = curl_exec($this->curl);
+
         return $response;
     }
 
-    protected function enviar(string $json): bool {
-        $response = $this->exec($json);
+    /**
+     * Send SMS using the Zenvia API.
+     *
+     * @param string $json JSON data to send.
+     * @return bool True if the SMS was sent successfully, false otherwise.
+     */
+    protected function send(string $json): bool {
+        $response = $this->execute($json);
 
         $this->response = json_decode($response);
 
         if (isset($this->response->sendSmsResponse)) {
             $this->response = $this->response->sendSmsResponse;
-            return (bool) ((int) $this->response->statusCode == self::STATUS_CODE_OK);
-        } else
+            return (bool)((int)$this->response->statusCode == self::STATUS_CODE_OK);
+        } else {
             return false;
+        }
     }
 
-    public function envioUnico(int $numero, string $mensagem, \DateTime $data): bool {
-        $nPara = self::DDI_BR . $numero;
+    /**
+     * Send a single SMS.
+     *
+     * @param int $number Recipient's phone number.
+     * @param string $message SMS message.
+     * @param \DateTime $date Scheduled date for sending the SMS.
+     * @return bool True if the SMS was sent successfully, false otherwise.
+     */
+    public function sendSingle(int $number, string $message, \DateTime $date): bool {
+        $toNumber = self::DDI_BR . $number;
 
         $sendSmsRequest = array(
-            "from" => $this->remetente,
-            "to" => $nPara,
-            "schedule" => $data->format('Y-m-d'),
-            "msg" => $mensagem,
+            "from" => $this->sender,
+            "to" => $toNumber,
+            "schedule" => $date->format('Y-m-d'),
+            "msg" => $message,
             "callbackOption" => "ALL",
         );
 
         $json = json_encode(['sendSmsRequest' => $sendSmsRequest]);
-        return $this->enviar($json);
+        return $this->send($json);
     }
 
-    function getResponse() {
+    /**
+     * Get the API response.
+     *
+     * @return object|null API response.
+     */
+    public function getResponse(): ?object {
         return $this->response;
     }
 
+    /**
+     * Destructor to close the cURL resource.
+     */
+    public function __destruct() {
+        if ($this->curl) {
+            curl_close($this->curl);
+        }
+    }
 }
